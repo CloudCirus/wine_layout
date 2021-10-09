0d48ee8b0c4af91908efeb8337bed70950e2ab10
Cloud <smirnovceo@yandex.ru>
2021-10-09 17:25:17 +0500

    Refactoring for easier reading

--- main.py
+++ main.py
@@ -9,18 +9,21 @@ from pandas import read_excel
 def count_company_age() -> int:
     FOUNDATION_YEAR = 1920
     now_year = datetime.datetime.now().year
+    
     return now_year - FOUNDATION_YEAR
 
 
 def get_format_data_from_xlsx(file_name: str, columns: list) -> defaultdict:
-
     wines = read_excel(file_name, usecols=columns,
                        keep_default_na=False).to_dict(orient='record')
+
     wines_by_category = defaultdict(list)
     for elem in wines:
         wines_by_category[elem.pop('Категория')].append(elem)
+
     wines_by_category = dict(
         sorted(wines_by_category.items(), key=lambda x: x[0]))
+
     return wines_by_category
 
 
@@ -39,12 +42,13 @@ def main() -> None:
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
-    template = env.get_template('template.html')
 
+    template = env.get_template('template.html')
     rendered_page = template.render(
         company_age=count_company_age(),
-        wines=wines_by_category
+        wines=wines_by_category,
     )
+
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
